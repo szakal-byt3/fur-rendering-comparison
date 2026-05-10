f5828a4a258f4c3340d42637fc3107ebbb145c10
Luke S. <83680711+szakal-byt3@users.noreply.github.com>
2026-05-10 10:15:52 -0700

    Revert "Add final(?) implementation to main"

--- Paper/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# alternatively, can run the following commands in this order:
-#   pdflatex paper.tex
-#   biber paper
-#   pdflatex paper.tex
-#   pdflatex paper.tex
-latexmk -pdf paper.tex
--- Paper/clean.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rm *.aux *.bbl *.bcf *.blg *.fdb_latexmk *.fls *.log *.xml
